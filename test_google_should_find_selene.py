9a6fd5716f5862521d4304a44ab11e67b561bf51
Julia Mikhailova <108729025+IuliiaMikhailova@users.noreply.github.com>
2023-04-05 20:40:20 +0300

    Добавила фикстуры и новые тесты

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,21 @@
+import pytest
+import selene
 from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture
+def browser_open():
+    browser.open('https://google.com')
+    browser.driver.set_window_size(1024, 768)
+
+def test_google_successfull(browser_open):
+
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_google_unsuccessfull(browser_open):
+
+    browser.element('[name="q"]').should(be.blank).type('dkjfvndkjfn').press_enter()
+    assert ('[id="search"]') == ('Selene - User-oriented Web UI browser tests in Python')
+    print('Результатов нет')
